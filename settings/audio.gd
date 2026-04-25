d660d89bffa8799bc11a1280b73ff0260d8ce2d9
needleful <needleful@outlook.com>
2026-04-24 18:10:43 -0700

    Added voice option

--- settings/audio.gd
+++ settings/audio.gd
@@ -4,6 +4,7 @@ class_name AudioSettings
 @export var master_audio: Resource = AudioChannel.new('Master')
 @export var music: Resource = AudioChannel.new('Music')
 @export var sfx: Resource = AudioChannel.new('SFX')
+@export var voices: Resource = AudioChannel.new('Voice')
 
 var group_name := &'Audio'
 
