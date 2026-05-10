6471ce89cf6b9c217d2e3d41e5635a5279ea8e69
needleful <needleful@outlook.com>
2026-05-10 01:42:46 -0700

    Simplify

--- scripts/audio_channel.gd
+++ scripts/audio_channel.gd
@@ -4,24 +4,19 @@ class_name AudioChannel
 @export var vol: float:
 	set(v):
 		vol = v
-		apply()
+		AudioServer.set_bus_volume_db(index, percent_to_db(vol))
 @export var muted: bool:
 	set(m):
 		muted = m
-		apply()
+		AudioServer.set_bus_mute(index, muted)
 @export var bus_name: String
+
+var index: int
 
 func _init(name: String = ''):
 	resource_name = 'AudioChannel'
 	bus_name = name
-
-func apply():
-	var bi = AudioServer.get_bus_index(bus_name)
-	if vol == 0:
-		breakpoint
-	if bi >= 0:
-		AudioServer.set_bus_mute(bi, muted)
-		AudioServer.set_bus_volume_db(bi, percent_to_db(vol))
+	index = AudioServer.get_bus_index(bus_name)
 
 func percent_to_db(percent):
 	return 50*log(0.99*percent + 0.01)/log(10)
